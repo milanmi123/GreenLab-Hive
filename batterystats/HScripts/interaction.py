0b16f0e65b1b530788f50e13aa4f8e0828acb43b
Jaak Lin <linjaak@gmail.com>
2021-10-23 21:59:44 +0200

    RQ 2.2 signal strength raw data

--- batterystats/HScripts/interaction.py
+++
@@ -1,8 +0,0 @@
-from AndroidRunner.Device import Device
-# noinspection PyUnusedLocal
-def main(device, *args, **kwargs):
-    print('=INTERACTION=')
-    device.shell('input swipe 100 200 100 100 1000')
-    time.sleep(4)
-    print((device.id))
-    print((device.current_activity()))
